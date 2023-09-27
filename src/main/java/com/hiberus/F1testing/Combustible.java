689f39a9f0bfa9f8c5978f410ffe2a139ae77e80
Leticia Pilar Sánchez Romero <lpsanchez@hiberus.com>
2023-09-27 18:59:04 +0200

    added Combustible class + ideas

---
+++ src/main/java/com/hiberus/F1testing/Combustible.java
@@ -0,0 +1,8 @@
+package com.hiberus.F1testing;
+
+public class Combustible {
+
+    public TipoCombustible tipoCombustible;
+
+    public float litrosCombustible;
+}
--- src/main/java/com/hiberus/F1testing/Estrategia.java
+++ src/main/java/com/hiberus/F1testing/Estrategia.java
@@ -3,7 +3,7 @@ package com.hiberus.F1testing;
 
 public class Estrategia {
 
-    public TipoCombustible combustible;
+    public Combustible combustible;
     public float combustibleConsumidoPorKmRecorrido;
     public Neumaticos neumaticos;
     public float porcentajeDeVidaDeNeumaticosConsumidoPorKmRecorrido;
@@ -14,5 +14,20 @@ public class Estrategia {
         boolean estrategiaViable = false;
 
         return estrategiaViable;
+    }
+
+
+    private boolean sinCombustibleAntesFinRecorrido(Combustible combustible, float combustibleConsumidoPorKmRecorrido, float kilometrosARecorrer) {
+
+        // TODO: teniendo combustible (l);  recorrido (km); y combustible consumido por km. Calcular combustible al final de recorrido. Si <=0 entonces return true sino false
+
+        return false;
+    }
+
+    private boolean neumaticosDesgastadosAntesFinRecorrido(Neumaticos neumaticos, float porcentajeDeVidaDeNeumaticosConsumidoPorKmRecorrido, float kilometrosARecorrer) {
+
+        // TODO: teniendo porcentaje vida consumido por km; porcentaje inicial neumáticos; y recorrido (km). Calcular porcentaje al final de recorrido, si <=0 entonces return true sino false
+
+        return false;
     }
 }
